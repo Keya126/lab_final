2365a100c0ed02f72d73351cb25fea9097c220e1
keya126 <keyakamrunnahar78@gmail.com>
2025-04-15 15:30:09 +0600

    correcting remove function

--- studentManager.cpp
+++ studentManager.cpp
@@ -4,20 +4,27 @@
 
 class Student {
 private:
-    std::vector<std::string> Name;
+    std::vector<std::string> names;
 
 public:
     Student(std::string name){}
    void add_name(std::string name){
-    Name.push_back(name);
+    names.push_back(name);
    }
-   void remove_name(std::string name){
-    Name.pop_back();
-   }
+    void remove_name(const std::string& name) {
+        // Remove the first occurrence of the name
+        for (auto it = names.begin(); it != names.end(); ++it) {
+            if (*it == name) {
+                names.erase(it);
+                std::cout << "Removed: " << name << std::endl;
+                return;
+            }
+        }
+        std::cout << "Name not found: " << name << std::endl;
+    }
    
-   
-
 };
+
 
 
 
