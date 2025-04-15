e73ae5b665b7b8869e29be74c913d9fbcc437072
keya126 <keyakamrunnahar78@gmail.com>
2025-04-15 15:21:21 +0600

    add name function added

--- studentManager.cpp
+++ studentManager.cpp
@@ -6,7 +6,16 @@ class Student {
 private:
     std::vector<std::string> Name;
 
+public:
+    Student(std::string name){}
+   void add_name(std::string name){
+    Name.push_back(name);
+   }
+   
+   
+
 };
+
 
 
 
